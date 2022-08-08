67b470c13d97eb77f82425fcc07acf67cc162c29
Poitier Stringer <stringerpoitier@gmail.com>
2022-08-08 15:56:05 -0700

    add theme, customize pallete

---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+const theme = {
+    palette: {
+        primary: {
+            main: "#203A4C",
+            light: "#5FB4A2",
+            dark: "#33323D"
+
+        },
+        secondary: {
+            main: "#EAEAEB",
+            light: "#FAFAFA",
+        },
+        error: {
+            main: "#F43030",
+        },
+    }
+};
+export default theme;
